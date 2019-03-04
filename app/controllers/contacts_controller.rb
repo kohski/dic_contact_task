576dc61319171bdd4cfee0c234d12f039f4704b0
kosukekimura <kimkim5364@gmail.com>
2019-03-04 12:46:28 +0000

    DIC task practice1 rails application

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,10 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    Contact.create(name: params["name"],email: params["email"], content: params["content"])
+    redirect_to new_contact_path
+  end
+end
