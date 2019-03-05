25170f1596cc6b1477ad0ac03e0190e339697c5a
kosukekimura <kimkim5364@gmail.com>
2019-03-05 05:22:34 +0000

    use strong parameter in router.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,8 @@ class ContactsController < ApplicationController
   end
 
   def create
-    Contact.create(name: params["name"],email: params["email"], content: params["content"])
+    # Contact.create(name: params["name"],email: params["email"], content: params["content"])
+    Contact.create(params.permit(:name,:email,:content))
     redirect_to new_contact_path
   end
   
