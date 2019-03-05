3a2beaa275732e69123e5c7588943933a88054d6
kosukekimura <kimkim5364@gmail.com>
2019-03-05 02:13:12 +0000

    add valites for Contact Model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,4 +7,7 @@ class ContactsController < ApplicationController
     Contact.create(name: params["name"],email: params["email"], content: params["content"])
     redirect_to new_contact_path
   end
+  
+  private
+  
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ApplicationRecord
+  validates :content, length:{ minimum:1, maximum:140 }
 end
